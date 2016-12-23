a062dc6a8d2f769d0c1ddb8fcbcfcfcf54959c07
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-12-22 23:57:49 -0800

    added captcha and finalize maps

--- app.js
+++
@@ -1,9 +0,0 @@
-geocoder = new google.maps.Geocode();
-
-window.getCoordinates = function(address, callback){
-  var coordinates;
-  geocoder.geocode({addres: address}, function (results,status){
-   coordinates = results[0].geometry.location;
-   callback (coordinates);
-  })
-}
