9bfa46b25608137109f1b81f6ab46a5a06ac8035
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-12-23 19:17:33 -0800

    testing on school server

--- verification.php
+++ verification.php
@@ -1,18 +1,13 @@
+<?php
+  $firstName = $_POST["firstName"];
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <title="results">
   </head>
   <body>
-    <script type="text/javascript">
-
-    var firstname = <?php echo($_POST["firstName"]); ?>;
-    var replacedFirstName = firstname.replace(/%2D/g, "-");
-    var replacedFirstName = replacedFirstName.replace(/%2C/g, "'");
-    var replacedFirstName = replacedFirstName.split('+').join(' ');
-    document.write('<h3>test if it shows this text</h3>');
-    </script>
-
+    <h1><?php echo $firstName;?></h1>
 
 
   </body>
