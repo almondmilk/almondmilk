cde55aa939dee62c93998aec2b764202c78fc469
Alvin Lu <alvinlu@mail.sfsu.edu>
2016-12-23 21:35:54 -0800

    shit

--- verification.php
+++ verification.php
@@ -67,55 +67,54 @@ $firstName = $_POST['firstName'];
     <div class="page-header">
       <h2 style='text-align: center;'>Please verify your submission</h2>
     </div>
+    
     <div class="row">
       <?php
-      $target_dir = "uploads/";
-      $file_ary = $_FILES["fileToUpload"];
-      foreach($file_ary as $file){
-        $target_file = $target_dir . basename($file["name"]);
+      $target_dir = 'uploads/';
+
+        $target_file = $target_dir.basename($_FILES['fileToUpload']['name']);
         $uploadOk = 1;
-        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
         // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-          $check = getimagesize($file["tmp_name"]);
-          if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-          } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-          }
+        if (isset($_POST['submit'])) {
+            $check = getimagesize($_FILES['fileToUpload']['tmp_name']);
+            if ($check !== false) {
+                echo 'File is an image - '.$check['mime'].'.';
+                $uploadOk = 1;
+            } else {
+                echo 'File is not an image.';
+                $uploadOk = 0;
+            }
         }
         // Check if file already exists
         if (file_exists($target_file)) {
-          echo "Sorry, file already exists.";
-          $uploadOk = 0;
+            echo 'Sorry, file already exists.';
+            $uploadOk = 0;
         }
         // Check file size
-        if ($file["size"] > 5000000) {
-          echo "Sorry, your file is too large.";
-          $uploadOk = 0;
+        if ($_FILES['fileToUpload']['size'] > 5000000) {
+            echo 'Sorry, your file is too large.';
+            $uploadOk = 0;
         }
         // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-          echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-          $uploadOk = 0;
+        if ($imageFileType != 'jpg' && $imageFileType != 'png' && $imageFileType != 'jpeg'
+        && $imageFileType != 'gif') {
+            echo 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.';
+            $uploadOk = 0;
         }
         // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
-          echo "Sorry, your file was not uploaded.";
+            echo 'Sorry, your file was not uploaded.';
           // if everything is ok, try to upload file
         } else {
-          if (move_uploaded_file($file["tmp_name"], $target_file)) {
-            echo "The file ". basename( $file["name"]). " has been uploaded.";
-            echo '<img src="' . $target_file . '" alt="">';
-          } else {
-            echo "Sorry, there was an error uploading your file.";
-          }
+            if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+                echo 'The file '.basename($_FILES['fileToUpload']['name']).' has been uploaded.';
+                echo '<img src="'.$target_file.'" alt="">';
+            } else {
+                echo 'Sorry, there was an error uploading your file.';
+            }
         }
 
-      }
       /* Below is the single-file version of the image upload code.
       *
       $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -166,10 +165,10 @@ echo "Sorry, there was an error uploading your file.";
 <div class="row">
   <h5 style="text-align: right;" class="col-xs-3 control-h5" style="text-align: right;">First Name</h5>
   <h5 style="text-align: left;" class="col-xs-4 control-h5" style="text-align: right;">
-    <?php echo $firstName;?>
+    <?php echo $firstName; ?>
     <br>
     <script>
-    document.write('<?php echo $firstName;?>');
+    document.write('<?php echo $firstName; ?>');
     </script>
   </h5>
 </div>
